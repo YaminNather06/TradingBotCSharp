f6ddf168e92ec97f4eb8f85c030711012626d605
Yamin Nather <2001s.yn@gmail.com>
2022-02-22 17:50:20 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using TradingBotCSharp.DataAccess.Gateways;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        ITradingDataGateway tradingDataGateway = new CsvTradingDataGateway();
+
+        tradingDataGateway.GetDay1HistoricalDataOfPeriod();
+    }
+}
